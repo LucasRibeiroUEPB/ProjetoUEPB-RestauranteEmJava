069d653dbe21823d61ce7d7fcc2a60371d5baaf1
Lucas <Lucas@DESKTOP-B4FUP8B>
2026-05-28 17:32:28 -0300

    classe pedido finalizada

--- src/sistemaRestaurante/Pedido.java
+++ src/sistemaRestaurante/Pedido.java
@@ -1,5 +1,18 @@
 package sistemaRestaurante;
 
 public class Pedido {
-
+	String nomeDoItem;
+	double valorDoItem;
+	int	quantidade;
+	Pedido(String nomeDoItem, double valorDoItem, int quantidade){
+		this.nomeDoItem = nomeDoItem;
+		this.valorDoItem = valorDoItem;
+		this.quantidade = quantidade;
+	}
+	double calculaValorTotalDoPedido() {
+		return valorDoItem * quantidade;
+	}
+	public String toString() {
+		return "Pedido: " + nomeDoItem + " Valor Unitário R$: " + valorDoItem + " Quantidade: " + quantidade + " Total: R$" + calculaValorTotalDoPedido(); 
+	}
 }
