9b61886095ec22a237cdd3a626426bb135a7ef68
Ptavio <otavioformal@gmail.com>
2026-05-28 19:12:02 -0300

    teste de save

--- src/sistemaRestaurante/Mesa.java
+++ src/sistemaRestaurante/Mesa.java
@@ -1,5 +1,16 @@
 package sistemaRestaurante;
 
 public class Mesa {
+	int numero_de_mesa;
+	boolean mesa_ativa;
+	Pedido[] pedidos_da_mesa;
+	int Total_mesa;
+	public Mesa(int numero, int capacidade_de_pedidos) {
+		this.numero_de_mesa = numero;
+		this.mesa_ativa = true;
+		this.pedidos_da_mesa = new Pedido[capacidade_de_pedidos];
+		this.Total_mesa = 0;
+	}
+	//public boolean Adicionar_Pedido();
 
 }
