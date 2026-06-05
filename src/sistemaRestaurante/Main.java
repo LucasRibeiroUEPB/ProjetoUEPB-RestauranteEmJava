10ea188a689d9d2c87a270e89ca84306b1dc62d2
Administrador <Administrador@PE03E3GY>
2026-06-05 19:11:10 -0300

    Adicionado os métodos lerInteiro, lerTexto, lerDouble e mostrarMenu.

--- src/sistemaRestaurante/Main.java
+++ src/sistemaRestaurante/Main.java
@@ -1,9 +1,38 @@
 package sistemaRestaurante;
 
+import java.util.Scanner;
+
 public class Main {
 
+	static String lerTexto(String mensagem) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(mensagem);
+		return scan.nextLine();
+	}
+
+	static int lerInteiro(String mensagem) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(mensagem);
+		return scan.nextInt();
+	}
+
+	static double lerDouble(String mensagem) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(mensagem);
+		return scan.nextDouble();
+	}
+
+	static void mostrarMenu() {
+		System.out.println("=== Sistema Restaurante ===\n");
+		System.out.println("1 - Abrir Mesa");
+		System.out.println("2 - Adicionar Pedido");
+		System.out.println("3 - Fechar Mesa");
+		System.out.println("4 - Listar Mesas Ativas");
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int quantidade = lerInteiro("Insira a quantidade máxima de mesas do restaurante:");
+		Restaurante restaurante = new Restaurante(quantidade);
 
 	}
 
